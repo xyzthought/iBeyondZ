392d961fc2fa1a1aee7af27a92a9d310e75e28a0
xyzthought <xyz.thought@gmail.com>
2012-08-25 18:47:30 +0530

    tt

--- CSWeb/Default.aspx.cs
+++ CSWeb/Default.aspx.cs
@@ -13,6 +13,7 @@ public partial class _Default : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         ClearPage();
+        //ki je hoche
     }
 
     private void ClearPage()
